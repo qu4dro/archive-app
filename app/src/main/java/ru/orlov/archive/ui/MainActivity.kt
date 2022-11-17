dff8094bdb270fe9a509599807c6aa46340428cb
Pete <pete@MacBook-Pro-Mac-ovv.local>
2022-11-17 15:29:32 +0300

    Bottom navigation

--- app/src/main/java/ru/orlov/archive/ui/MainActivity.kt
+++ app/src/main/java/ru/orlov/archive/ui/MainActivity.kt
@@ -2,11 +2,28 @@ package ru.orlov.archive.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import ru.orlov.archive.R
+import ru.orlov.archive.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setNavController()
     }
+
+    private fun setNavController() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.findNavController()
+        binding.bottomNavigation.setupWithNavController(navController)
+    }
+
+
 }
